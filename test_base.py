bb76918ae94224cad709d41c21e35d8536f2674d
Andrey <sidorov.avk@gmail.com>
2021-03-02 21:42:13 +0400

    Base test

---
+++ test_base.py
@@ -0,0 +1,5 @@
+class TestAndroidBasicInteractions():
+
+    def test_should_send_keys_to_search_box_and_then_check_the_value(self, driver):
+        search_box_element = driver.find_element_by_id('txt_query_prefill')
+        search_box_element.send_keys('Hello world!')
